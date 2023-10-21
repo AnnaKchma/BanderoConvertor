1a1177121590e7f8df3738df3e8a9e627e98af6b
AnnaKchma <anna_kuchma09@ukr.net>
2023-10-21 23:49:01 +0200

    added Main class update classes on GitHub renamed deleted De reworked NotificationSender in a way that is sends notifications every working day at 9:00

--- src/main/java/org/example/NotificationSender.java
+++ src/main/java/org/example/NotificationSender.java
@@ -3,55 +3,42 @@ package org.example;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.Calendar;
 
 public class NotificationSender {
+    // Create a scheduler
     private final ScheduledExecutorService scheduler;
 
     public NotificationSender() {
         // Create a scheduler
         scheduler = Executors.newScheduledThreadPool(1);
 
-        // Schedule notifications at specific times (e.g., 9:00 AM)
-        scheduleNotification();
+        // Schedule notifications at 9:00 AM every working day
+        scheduleNotifications();
     }
 
-    private void scheduleNotification() {
-        // Calculate the delay until the next occurrence of the specified time
-        long initialDelay = calculateInitialDelay();
-
+    private void scheduleNotifications() {
         scheduler.scheduleAtFixedRate(() -> {
-            // Send the notification to users here
-        }, initialDelay, 24, TimeUnit.HOURS);
-    }
+            // Get the current time
+            Calendar now = Calendar.getInstance();
+            int currentHour = now.get(Calendar.HOUR_OF_DAY);
+            int currentMinute = now.get(Calendar.MINUTE);
+            int dayOfWeek = now.get(Calendar.DAY_OF_WEEK);
 
-    private long calculateInitialDelay() {
-        // Calculate the initial delay until the specified time
-        long currentMillis = System.currentTimeMillis();
-        long targetMillis = getTimeInMillis();
 
-        // Calculate the delay in milliseconds
-        long initialDelay = targetMillis - currentMillis;
-
-        if (initialDelay < 0) {
-            // If the specified time has already passed today, add 24 hours to schedule for tomorrow
-            initialDelay += TimeUnit.HOURS.toMillis(24);
-        }
-
-        return initialDelay;
-    }
-
-    private long getTimeInMillis() {
-        long currentTime = System.currentTimeMillis();
-        long currentHour = TimeUnit.MILLISECONDS.toHours(currentTime);
-        long currentMinute = TimeUnit.MILLISECONDS.toMinutes(currentTime - TimeUnit.HOURS.toMillis(currentHour));
-
-        long targetTime = TimeUnit.HOURS.toMillis(9) + TimeUnit.MINUTES.toMillis(0);
-
-        // Calculate the target time in milliseconds
-        return currentTime - TimeUnit.HOURS.toMillis(currentHour) - TimeUnit.MINUTES.toMillis(currentMinute) + targetTime;
+            if (dayOfWeek >= Calendar.MONDAY && dayOfWeek <= Calendar.FRIDAY &&
+                    currentHour == 9 && currentMinute == 0) {
+                // Send the notification to users here
+                System.out.println("Sending notification at 9:00 AM");
+            }
+        }, 0, 1, TimeUnit.MINUTES);
     }
 
     public static void main(String[] args) {
-        new NotificationSender();
+        try {
+            new NotificationSender();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
